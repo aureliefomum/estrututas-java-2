1cbd48d782e84a0c2a0ded6b86e605b1e5052bb2
aureliefomum <azam_aurelie@yahoo.com>
2022-03-19 22:29:54 -0300

    created getNo method on double linked list

--- listaDuplamenteEncadeada/src/main/java/one/digitalinnovation/ListaDuplamenteEncadeada.java
+++ listaDuplamenteEncadeada/src/main/java/one/digitalinnovation/ListaDuplamenteEncadeada.java
@@ -15,6 +15,20 @@ public class ListaDuplamenteEncadeada <T> {
         this.tamanhoLista = 0;
     }
 
+    //create getNo method which you will later use in other methods like remove, add, etc
+    private NoDuplo<T> getNo(int index){
+        //save first node in noAuxiliar
+        NoDuplo<T> noAuxiliar = primeiroNo;
+
+        //create a for loop with two stop conditions: if i is less than given index and if noAuxiliar is not null
+
+        for(int i =0; (i < index) && (noAuxiliar !=null);i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        //Once loop finishes running, return NoAuxiliar
+        return noAuxiliar;
+    }
+
 
     public int size(){
         return tamanhoLista;
