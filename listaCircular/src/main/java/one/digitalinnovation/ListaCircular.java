c4d6acead769a03b72990155e34ad47034449f28
aureliefomum <azam_aurelie@yahoo.com>
2022-03-25 13:25:49 -0300

    created circular list Class

---
+++ listaCircular/src/main/java/one/digitalinnovation/ListaCircular.java
@@ -0,0 +1,33 @@
+package one.digitalinnovation;
+
+public class ListaCircular<T> {
+
+    private No<T> cabeca;
+    private No<T> cauda;
+    private No<T> tamanhoLista;
+
+
+
+    private No<T> getNo(int index){
+        if(this.isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vazia!");
+        if(index == 0){
+            return this.cauda;
+        }
+        No<T> noAuxiliar = this.cauda;
+        for(int i = 0; (i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+
+        return noAuxiliar;
+    }
+
+    public boolean isEmpty(){
+        return this.tamanhoLista == 0 ? true: false;
+    }
+
+    public int size(){
+        return this.tamanhoLista;
+    }
+
+}
