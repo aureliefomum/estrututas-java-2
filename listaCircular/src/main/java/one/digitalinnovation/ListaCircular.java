4af9555e5d7bbc9c07b256ca5189a05903e04b13
aureliefomum <azam_aurelie@yahoo.com>
2022-03-25 13:51:30 -0300

    created 'remove' method on circular list

--- listaCircular/src/main/java/one/digitalinnovation/ListaCircular.java
+++ listaCircular/src/main/java/one/digitalinnovation/ListaCircular.java
@@ -6,6 +6,28 @@ public class ListaCircular<T> {
     private No<T> cauda;
     private No<T> tamanhoLista;
 
+    public void remove(int index){
+        if(index >= this.tamanhoLista)
+            throw new IndexOutOfBoundsException("O indice é maior que o tamanho da lista!");
+        No<T> noAuxiliar = this.cauda;
+        //check if node to be removed is tail of list
+        if(index ==0){
+            //if yes, mke the next node, the tail node
+            this.cauda = this.cauda.getNoProximo();
+            //link the head to the new tail
+            this.cabeca.setNoProximo(this.cauda);
+        }else if(index==1){
+            this.cauda.setNoProximo(this.cauda.getNoProximo().getNoProximo());
+        }else{
+            //if index is neither 0 nor 1, loop through list up till node at given index
+            for(int=0; i < index -1;i++){
+                noAuxiliar = noAuxiliar.getNoProximo();
+            }
+            noAuxiliar.setNoProximo(noAuxiliar.getNoProximo().noAuxiliar.getNoProximo());
+        }
+        this.tamanhoLista--;
+    }
+
     public T get(int index){
         return this.getNo(index).getConteudo();
     }
