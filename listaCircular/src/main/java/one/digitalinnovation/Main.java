19e39617ebf6a2fe0d6c6a849effcdb821f109cb
aureliefomum <azam_aurelie@yahoo.com>
2022-03-26 15:46:05 -0300

    created 'Main' class for Circular List

---
+++ listaCircular/src/main/java/one/digitalinnovation/Main.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaCircular <String> minhaListaCircular = new ListaCircular<>();
+
+        minhaListaCircular.add("c0");
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.remove(0);
+        System.out.println(minhaListaCircular);
+    }
+    }
+
