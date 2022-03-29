a83ba3151fc4ab764c1fdf7126f7bf576fcde911
aureliefomum <azam_aurelie@yahoo.com>
2022-03-29 19:20:11 -0300

    created 'Main' class to run Circular List

--- listaCircular/src/main/java/one/digitalinnovation/Main.java
+++ listaCircular/src/main/java/one/digitalinnovation/Main.java
@@ -9,6 +9,25 @@ public class Main {
 
         minhaListaCircular.remove(0);
         System.out.println(minhaListaCircular);
+
+        minhaListaCircular.add("c1");
+        System.out.println("depois de adicionar c1: " + minhaListaCircular);
+
+        minhaListaCircular.add("c2");
+
+
+        minhaListaCircular.add("c3");
+        System.out.println("depois de adicionar c2 e c3: " + minhaListaCircular);
+
+        System.out.println("Elemento de indice 0: " + minhaListaCircular.get(0));
+        System.out.println("Elemento de indice 1: " + minhaListaCircular.get(1));
+
+        for(int i=0; i<=20; i++){
+            System.out.println("Elemento de indice " + i + ": " + minhaListaCircular.get(i));
+        }
     }
+
+
+
     }
 
