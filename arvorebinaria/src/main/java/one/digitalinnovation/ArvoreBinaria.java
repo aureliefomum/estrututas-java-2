7f1a4a5918ce6386b9a6c6a00e0a11306606a672
aureliefomum <azam_aurelie@yahoo.com>
2022-04-02 21:22:15 -0300

    implemented Binary Search Tree

---
+++ arvorebinaria/src/main/java/one/digitalinnovation/ArvoreBinaria.java
@@ -0,0 +1,27 @@
+package one.digitalinnovation;
+
+public class ArvoreBinaria<T extends Comparable<T>> {
+    private BinNo<T> raiz;
+
+    public ArvoreBinaria(){
+        this.raiz = null;
+    }
+
+    public void inserir(T conteudo){
+        BinNo<T> novoNo = new BinNo<>(conteudo);
+        //call the insert method below
+        raiz = inserir(raiz, novoNo);
+    }
+
+    private BinNo<T> inserir(BinNo<T> atual, BinNo<T> novoNo){
+        if(atual == null){
+            return novoNo;
+        }else if(novoNo.getConteudo().compareTo(atual.getConteudo()) < 0){
+            //compareTo returns -1 if novoNo is less than atual and 1 if it is more than atual
+            atual.setNoEsq(inserir(atual.getNoEsq(), novoNo));
+        }else{
+            atual.setNoDir(inserir(atual.getNoDir(), novoNo));
+        }
+        return atual;
+    }
+}
