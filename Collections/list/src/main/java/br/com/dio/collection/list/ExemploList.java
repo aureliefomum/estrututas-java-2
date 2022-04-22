fe5ae36edf2c2fdbd18374c0887665089146bcba
aureliefomum <azam_aurelie@yahoo.com>
2022-04-21 21:36:58 -0300

    ExemploOrdenacaoList

--- Collections/list/src/main/java/br/com/dio/collection/list/ExemploList.java
+++ Collections/list/src/main/java/br/com/dio/collection/list/ExemploList.java
@@ -1,23 +1,22 @@
 package br.com.dio.collection.list;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ExemploList {
     public static void main(String[] args) {
         // Dada uma lista com 7 notas de um aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça:
 
-     List notas = new ArrayList(); //antes do java 5
+    // List notas = new ArrayList(); //antes do java 5
 //      ArrayList<Double> notas = new ArrayList<>();
 //      List<Double> notas = new ArrayList<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
         /*List<Double> notas = Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
-        notas.add(10d);
+        notas.add(10d); (Vai dar erro porque não pode adicionar elementos)
         System.out.println(notas);*/
         /*List<Double> notas = List.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
         notas.add(1d);
         notas.remove(5d);
         System.out.println(notas);*/
-        System.out.println("Crie uma lista e adicione as sete notas: ");
+        /*System.out.println("Crie uma lista e adicione as sete notas: ");
 
         List<Double> notas = new ArrayList<Double>(); //Generics(jdk 5) - Diamond Operator(jdk 7)
         notas.add(7.0);
@@ -28,7 +27,8 @@ public class ExemploList {
         notas.add(0.0);
         notas.add(3.6);
         System.out.println(notas.toString());
-
+        System.out.println(notas);
+       //exiba a posição da nota 5.0
         System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5d));
 
         System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
@@ -41,8 +41,8 @@ public class ExemploList {
 
         System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5d));
 
-        /*System.out.println("Exiba todas as notas na ordem em que foram informados: ");
-        for (Double nota : notas) System.out.println(nota);*/
+        *//*System.out.println("Exiba todas as notas na ordem em que foram informados: ");
+        for (Double nota : notas) System.out.println(nota);*//*
 
         System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
         System.out.println(notas.toString());
@@ -75,21 +75,39 @@ public class ExemploList {
             Double next = iterator1.next();
             if(next < 7) iterator1.remove();
         }
-        System.out.println(notas);
+        System.out.println(notas);*/
 
         /*System.out.println("Apague toda a lista");
         notas.clear();
         System.out.println(notas);
         */
-        System.out.println("Confira se a lista está vazia: " + notas.isEmpty());
+        /*System.out.println("Confira se a lista está vazia: " + notas.isEmpty());*/
 
 /*
 Para você: Resolva esses exercícios utilizando os métodos da implementação LinkedList:
+
         System.out.println("Crie uma lista chamada notas2 " +
                 "e coloque todos os elementos da list Arraylist nessa nova lista: ");
+                List<Double> notas = new ArrayList<Double>();
         System.out.println("Mostre a primeira nota da nova lista sem removê-lo: ");
         System.out.println("Mostre a primeira nota da nova lista removendo-o: ");
 */
+
+        List<Double> notas2 = new LinkedList<>();
+        notas2.add(7.0);
+        notas2.add(8.5);
+        notas2.add(9.3);
+        notas2.add(5.0);
+        notas2.add(7.0);
+        notas2.add(0.0);
+        notas2.add(3.6);
+        System.out.println("Crie uma lista chamada notas2 " +
+                "e coloque todos os elementos da list Arraylist nessa nova lista: ");
+        System.out.println(notas2);
+
+        System.out.println("Mostre a primeira nota da nova lista sem removê-lo: " + notas2.get(0));
+
+        System.out.println("Mostre a primeira nota da nova lista removendo-o: " + ((LinkedList<Double>) notas2).removeFirst());
 
     }
 }
